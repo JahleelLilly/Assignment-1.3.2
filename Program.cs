9be294d93b5ac211a66cecc34989d3bea3c59255
Jahle <Jahle@ABLACKMAN>
2024-05-09 15:14:56 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace Assignment_1._3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] newArray = { 2, 5, 7 };
+            int length = newArray.Length;
+            int temp = 0;
+
+
+            for (int i = 0; i < length / 2; i++)
+            {
+                temp = newArray[i];
+                newArray[i] = newArray[length - i - 1];
+                newArray[length - i - 1] = temp;
+
+            }
+
+            foreach (int i in newArray)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
